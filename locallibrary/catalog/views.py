dc9b690e6b55d64f35af2c7dcdfaa4918b6cbdf2
duongtn <Kh0ngc0pa$$>
2022-04-29 02:49:39 +0700

    init project

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,70 @@
+from django.http import Http404
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    paginate_by = 10
+    queryset = Book.objects.filter(title__icontains='w')[:5]
+    template_name = 'books/my_arbitrary_template_name_list.html'
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='w')[:5]
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, primary_key):
+        try:
+            book = Book.objects.get(pk=primary_key)
+        except Book.DoesNotExist:
+            raise Http404('Book does not exist')
+
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'
+    paginate_by = 10
+    template_name = 'authors/my_arbitrary_template_name_list.html'
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def author_detail_view(request, primary_key):
+        try:
+            author = Author.objects.get(pk=primary_key)
+        except Author.DoesNotExist:
+            raise Http404('Book does not exist')
+
+        return render(request, 'catalog/author_detail.html', context={'author': author})
